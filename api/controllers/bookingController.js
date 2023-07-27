41c7b79c533ce63da67092f60aa980642529d84c
yves <yvlison4@gmail.com>
2023-07-27 13:18:38 -0600

    completed the usecase to modify a particular booking

--- api/controllers/bookingController.js
+++ api/controllers/bookingController.js
@@ -1,13 +1,32 @@
 import Booking from "../models/Booking.js";
 
+//create Booking
 export const createBooking = async (req, res, next) => {
+  req.body.date = new Date();
+  req.body.status = "Pending";
   const newBooking = new Booking(req.body);
-  console.log(newBooking);
   try {
     const saveBooking = await newBooking.save();
     res.status(200).json(saveBooking);
   } catch (err) {
     console.log("Development bug: issue with booking");
+    next(err);
+  }
+};
+
+export const modifyBooking = async (req, res, next) => {
+  try {
+    const modifyBooking = await Booking.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(modifyBooking);
+  } catch (err) {
     next(err);
   }
 };
--- api/routes/booking.js
+++ api/routes/booking.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createBooking } from "../controllers/bookingController.js";
+import {
+  createBooking,
+  modifyBooking,
+} from "../controllers/bookingController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-//create booking
+//booking routes
 router.post("/bookNow", createBooking);
+router.put("/modify/:id", verifyAdmin, modifyBooking);
 
 export default router;
