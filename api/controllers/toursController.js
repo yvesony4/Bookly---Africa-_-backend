df80b70af2777f8257e857c66da151fcdccbe7a4
yves <yvlison4@gmail.com>
2023-08-01 15:15:45 -0600

    modify tour information including pricing,itenaries,inclusion or exclusion - deleting or view tours

--- api/controllers/toursController.js
+++ api/controllers/toursController.js
@@ -9,3 +9,43 @@ export const createTour = async (req, res, next) => {
     next(err);
   }
 };
+
+export const updateTour = async (req, res, next) => {
+  try {
+    const updatedTour = await Tours.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedTour);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteTour = async (req, res, next) => {
+  try {
+    await Tours.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({ responseCode: 200, message: "Tour deleted successfully" });
+  } catch (err) {
+    res
+      .status(502)
+      .json({ responseCode: 502, message: "Something went wrong!" });
+    //next(err);
+  }
+};
+
+export const getTours = async (req, res, next) => {
+  try {
+    const tours = await Tours.find();
+    res.status(200).json(tours);
+  } catch (err) {
+    next(err);
+  }
+};
